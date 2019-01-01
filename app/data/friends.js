5bda9504d8347bbb3249e3b7f80bb9e1315914e8
Luis Cartagena <luiscartagena2017@u.northwestern.edu>
2018-12-31 19:14:49 -0800

    updated friends api

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,10 +7,64 @@
 
 const friendsArray = [
   {
-    name: "Ahmed",
+    name: "Ganon",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://vignette.wikia.nocookie.net/zelda/images/1/18/Ganon_%28Oracle_of_Ages_%26_Oracle_of_Seasons%29.png/revision/latest?cb=20100413011018",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    //32
+  },
+  {
+    name: "Bowser",
+    photo:
+      "https://i.pinimg.com/originals/5f/c2/ab/5fc2ab3ccfdc99f50e751b80604ea28f.png",
+    scores: [2, 5, 1, 3, 5, 3, 4, 3, 2, 2]
+    //30
+  },
+  {
+    name: "Wario",
+    photo:
+      "https://aff5fa4925746bf9c161-fb36f18ca122a30f6899af8eef8fa39b.ssl.cf5.rackcdn.com/images/Masthead_Wario.5fb367ea.png",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    //50
+  },
+  {
+    name: "Princess Peach",
+    photo:
+      "https://aff5fa4925746bf9c161-fb36f18ca122a30f6899af8eef8fa39b.ssl.cf5.rackcdn.com/images/Masthead_Peach.5fb367ea.png",
+    scores: [1, 5, 1, 2, 1, 1, 1, 1, 1, 1]
+    //15
+  },
+  {
+    name: "Toad",
+    photo:
+      "https://vignette.wikia.nocookie.net/nintendo/images/0/01/Toad_NSMBW.png/revision/latest?cb=20120110143846&path-prefix=en",
+    scores: [3, 4, 3, 2, 2, 4, 1, 2, 2, 2]
+    //25
+  },
+  {
+    name: "Mario",
+    photo:
+      "http://mario.nintendo.com/assets/img/home/char-mario.png",
+    scores: [2, 5, 4, 3, 1, 4, 5, 3, 4, 4]
+    //35
+  },
+  {
+    name: "Samus Aran",
+    photo: "https://images-na.ssl-images-amazon.com/images/I/41oC9uyPY2L._SL500_AC_SS350_.jpg",
+    scores: [3, 4, 4, 5, 5, 5, 3, 5, 3, 3]
+    //40
+  },
+  {
+    name: "Link",
+    photo: "https://vignette.wikia.nocookie.net/zelda/images/2/2a/Link_Hyrule_Warriors.png/revision/latest?cb=20140620205907",
+    scores: [3, 4, 4, 5, 5, 5, 3, 5, 3, 3]
+    //40
+  },
+  {
+    name: "Princess Zelda",
+    photo: "https://vignette.wikia.nocookie.net/zelda/images/5/5f/Zelda_Artwork_%28Breath_of_the_Wild%29.png/revision/latest?cb=20170306081400",
+    scores: [3, 4, 4, 5, 5, 5, 3, 5, 3, 3]
+    //40
   }
 ];
 
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // (ex. User fills out the survey... this data is then sent to the server...
   // Then the server saves the data to the friendsData array)
   // ---------------------------------------------------------------------------
 
